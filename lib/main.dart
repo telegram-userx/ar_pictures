72141191aa650a92d686cd1befd657e83176268c
Dev1 <directus.smtp@gmail.com>
2024-08-18 15:21:42 +0500

    add webview && permission_handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'src/presentation/application.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Permission.camera.request();
 
   runApp(const Application());
 }
