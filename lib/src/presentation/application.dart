1d59be2b86e3747fa72d7e8d41b51e68643019c5
Dev1 <directus.smtp@gmail.com>
2024-08-19 06:49:52 +0500

    refactor

--- lib/src/presentation/application.dart
+++ lib/src/presentation/application.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:turkmen_localization_support/turkmen_localization_support.dart';
 
-import 'home_screen.dart';
+import 'home/home_screen.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
