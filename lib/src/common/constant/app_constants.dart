60fa6796868696192c87d686782a12ff2ec3da54
Dev1 <directus.smtp@gmail.com>
2024-08-21 19:44:08 +0500

    add app constants

---
+++ lib/src/common/constant/app_constants.dart
@@ -0,0 +1,17 @@
+class AppConstants {
+  const AppConstants._();
+
+  static const double borderRadius = 20;
+
+  static const double padding = 12;
+
+  static const double fontSize = 14;
+
+  static const double fontSizeH1 = 20;
+  static const double fontSizeH2 = 18;
+  static const double fontSizeH3 = 16;
+
+  static const double fontSizeP1 = 14;
+  static const double fontSizeP2 = 12;
+  static const double fontSizeP3 = 10;
+}
