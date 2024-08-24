428f308d1fdc18b6cccbdbd78efc5a18a730204c
Dev1 <directus.smtp@gmail.com>
2024-08-24 22:49:54 +0500

    fix mobile scanner error

--- lib/src/presentation/screen/photo_album/mobile_scanner_screen.dart
+++ lib/src/presentation/screen/photo_album/mobile_scanner_screen.dart
@@ -5,12 +5,53 @@ import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:mobx/mobx.dart';
 
 import '../../../common/extension/extensions.dart';
+import '../../../common/services/permissions_service/permissions_service.dart';
 import '../../../service_locator/sl.dart';
 import 'store/photo_album_store.dart';
 
 @RoutePage()
-class MobileScannerScreen extends StatelessWidget {
+class MobileScannerScreen extends StatefulWidget {
   const MobileScannerScreen({super.key});
+
+  @override
+  State<MobileScannerScreen> createState() => _MobileScannerScreenState();
+}
+
+class _MobileScannerScreenState extends State<MobileScannerScreen> {
+  late final List<ReactionDisposer> _disposers;
+  late final MobileScannerController _scannerController;
+
+  @override
+  void initState() {
+    _scannerController = MobileScannerController(
+      detectionSpeed: DetectionSpeed.normal,
+      detectionTimeoutMs: 1000,
+    );
+
+    _disposers = [
+      reaction(
+        (_) => sl<PermissionsService>().hasCameraPermission,
+        (hasCameraPermission) {
+          if (!hasCameraPermission.value) {
+            sl<PermissionsService>().requestCameraPermission();
+          }
+        },
+      )
+    ];
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scannerController.dispose();
+
+    for (final dispose in _disposers) {
+      dispose();
+    }
+
+    super.dispose();
+  }
 
   void _handleBarcode(BarcodeCapture barcodes) {
     final barcode = barcodes.barcodes.firstOrNull;
@@ -41,6 +82,7 @@ class MobileScannerScreen extends StatelessWidget {
           }
 
           return MobileScanner(
+            controller: _scannerController,
             onDetect: _handleBarcode,
           );
         }),
