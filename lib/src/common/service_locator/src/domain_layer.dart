f1fb10b9f79fddf6cb9c19549f24c1c8fedd182b
Dev1 <directus.smtp@gmail.com>
2024-09-02 23:03:43 +0500

    update initialization

--- lib/src/common/service_locator/src/domain_layer.dart
+++ lib/src/common/service_locator/src/domain_layer.dart
@@ -1,3 +1,16 @@
 part of '../sl.dart';
 
-Future<void> _initDomainLayer() async {}
+Future<void> _initDomainLayer() async {
+  sl.registerSingleton<PhotoAlbumRepository>(
+    PhotoAlbumRepositoryImpl(
+      remotePhotoAlbumRepository: RemotePhotoAlbumRepositoryImpl(
+        albumSdk: sl<PhotoAlbumSdk>(),
+        arVideoSdk: sl<ArVideoSdk>(),
+      ),
+      localPhotoAlbumRepository: LocalPhotoAlbumRepositoryImpl(
+        albumDao: sl<PhotoAlbumDao>(),
+        arVideoDao: sl<ArVideoDao>(),
+      ),
+    ),
+  );
+}
--- lib/src/common/service_locator/src/presentation_layer.dart
+++ lib/src/common/service_locator/src/presentation_layer.dart
@@ -4,4 +4,10 @@ Future<void> _initPresentationLayer() async {
   sl.registerSingleton<AppRouter>(
     AppRouter(),
   );
+
+  sl.registerSingleton<QrScannerStore>(
+    QrScannerStore(
+      albumRepository: sl<PhotoAlbumRepository>(),
+    ),
+  );
 }
